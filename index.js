91450dcf4774fe745b7530c91cc3b60ed5dc6e7f
Gabriella <tangredig03@gmail.com>
2025-09-20 12:00:58 -0600

    Implement burgers and featuredDrink scope lab solution

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// Step 1: Variable Data (global scope)
+let burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Step 2: Function and Block
+function addBurger() {
+  const newBurger = 'Flatburger';   // function-scoped
+  burgers.push(newBurger);          // add to burgers in this order
+}
+
+// Step 3: Variable Declaration and Scope
+if (true) {                         // always executes
+  let anotherNewBurger = 'Maple Bacon Burger'; // block-scoped
+  burgers.push(anotherNewBurger);              // then push
+}
+
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
+
+// export for tests
+module.exports = { burgers, featuredDrink, addBurger, changeFeaturedDrink };
